e209c6b9c58bab1fb66bbf5200a22e9d400964d7
Crowdbotics <engineering@crowdbotics.com>
2023-03-05 00:10:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-band-dev-67843",
+  url: "https://hidden-band-dev-67843.botics.co",
+  api: "https://hidden-band-dev-67843.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
